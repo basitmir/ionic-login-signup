5b29069f67794b0440d86aaf3281bde2a2abcb08
Basit Mir <iammirbasit@gmail.com>
2018-11-23 15:40:38 +0530

    save the componets folder

---
+++ src/components/components.module.ts
@@ -0,0 +1,11 @@
+import {NgModule}  from '@angular/core';
+import {IonicModule} from 'ionic-angular';
+
+@ngModule({
+    declarations : [],
+    imports : [IonicModule],
+    exports :[]
+})
+export class ComponentsModule{
+
+}
