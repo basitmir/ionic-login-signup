668012da23e477d9509c2e0ba1541b8060d24fb0
Basit Mir <iammirbasit@gmail.com>
2018-11-24 23:05:11 +0530

    setting up firebase for authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+import { FIREBASE_CONFIG } from './app.firebase.config';
 
 //import { HomePage } from '../pages/home/home';
 @Component({
@@ -11,6 +13,7 @@ export class MyApp {
   rootPage:string = 'LoginPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp(FIREBASE_CONFIG);
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
+
 //import { HomePage } from '../pages/home/home';
 
 @NgModule({
@@ -14,7 +16,8 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+     
   ],
   bootstrap: [IonicApp],
   entryComponents: [
