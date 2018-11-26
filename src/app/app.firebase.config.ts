14c70466c548b927500eb3f049f756b18cda7ad1
Basit Mir <iammirbasit@gmail.com>
2018-11-26 21:35:47 +0530

    Update app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,7 +1,7 @@
 export const FIREBASE_CONFIG ={
     
-        apiKey: "AIzaSyDGWZVi54N_242z9KORDs9Ffvha4YqKMEE",
-        authDomain: "login-6ffe7.firebaseapp.com",
+        apiKey: "<<place your api key>> ",
+        authDomain: "<<place authDomain>>",
         
     
       
